d1f2a2eb8a3dcb70d5c62a2268a43f2e1428172a
kirukufaith5-collab <kirukufaith5@gmail.com>
2026-07-29 21:03:30 +0300

    Create models for user,Famer and Order

--- app/models.py
+++ app/models.py
@@ -1,14 +1,28 @@
-from flask_sqlalchemy import SQLAlchemy
+from app import db
+from datetime import datetime
 
-db = SQLAlchemy()
+#User model (For farmer and Admins)
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash =db.Column(db.String(256),nullable=False)
-    role =db.Column(db.String(20),nullable=False)
-    def set_password(self,password):
-        self.password_hash =generate_password_hash(password)
-     def check_password(self,password):
-        
+class User(db.model):
+    id =db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(50),nullable =False, unique =True)
+    role =db.column(db.String(20),nullable =False)#Admin or Farmer
+
+    #Batch model (Farmer Deliveries)
+class Batch(db.Model):
+    id =db.Column(db.Integer,primary_key=True)
+    created_at = db.Column(db.String(20),default=datetime.utcnow().strftime('%Y-%m-%d'))
+    farmer_id =db.Column(db.Integer,db.Foreignkey('user.id'),nullable =False)
+    weight = db.Column(db.Float,nullable=True)
+    notes = db.Column(db.String(100)default='-')
+status =db.Column(db.String(20),default ='PENDING')
+
+#Order model (Client B2B sales)
+class Order(db.Model):
+    id =db.Column(db.Integer ,primary_key =True)
+    date =db.Column(db.String(20),default=datetime.utcnow().strftime(%Y-%m-%d))
+    client_name =db.Column(db.String(100),nullable=False)
+    product_type =db.Column(db.String(50),nullable=False)
+    quantity =db.Column(db.Float,nullable=False)
+    unit_price =db.Column(db.Float,nullable=False)
+    status=db.Column(db.String(20),default='ACTIVE')
