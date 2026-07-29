24780953c4a0183e8728b8d9aa8158449bfaa30d
kirukufaith5-collab <kirukufaith5@gmail.com>
2026-07-29 21:13:15 +0300

    Create Schemas.py file and serialize both Batch and Order data

---
+++ app/schemas.py
@@ -0,0 +1,21 @@
+from app import ma
+from app.models import User, Batch,Order
+
+#Schema to serialize Batch data
+class BatchSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model =Batch
+        load_instance =True
+
+#Schema to serialize Order data
+class OrderSchema(ma.SQLAlchmeyAutoSchema):
+    class Meta:
+        model =Order
+        load_instance =True
+
+#Instantiate schemas
+batch_schema =BatchSchema()
+batches_schema =BatchSchema(many=True)
+
+order_schema =OrderSchema()
+orders_schema =OrderSchema(many =True)
