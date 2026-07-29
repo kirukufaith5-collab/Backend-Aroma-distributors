2869a11b8f9591fc2aadc36c98aceae9c284d26d
kirukufaith5-collab <kirukufaith5@gmail.com>
2026-07-30 00:55:58 +0300

    Add database drivers e,g gunicon in requirements.txt file

--- app/Farmer/routes.py
+++ app/Farmer/routes.py
@@ -3,7 +3,7 @@ from app import db
 from app.models import Batch
 from app.schemas import batches_schema ,batch_schema
 
-farmer_bp = Blueprint('farmer', __name__)
+farmer_bp = Blueprint('farmer_bp', __name__)
 
 # GET: Fetch batches for a specific farmer
 @farmer_bp.route('/<int:farmer_id>/batches', methods=['GET'])
@@ -12,15 +12,19 @@ def get_farmer_batches(farmer_id):
     return jsonify(batches_schema.dump(batches)), 200
 
 # POST: Log new harvest batch
+# Farmer creates a new batch
 @farmer_bp.route('/batches', methods=['POST'])
-def add_batch():
-    data = request.json
+def create_batch():
+    data = request.get_json()
+    
     new_batch = Batch(
-        farmer_id=data['farmer_id'],
-        product_type=data['product_type'],
-        weight=float(data['weight']),
-        notes=data.get('notes', '—')
+        farmer_id=data.get('farmer_id'),
+        product_type=data.get('coffee_type'),
+        quantity_kg=data.get('quantity_kg'),
+        notes=data.get('notes', '')
     )
+    
     db.session.add(new_batch)
     db.session.commit()
-    return jsonify(batch_schema.dump(new_batch)), 201
+    
+    return batch_schema.jsonify(new_batch), 201
