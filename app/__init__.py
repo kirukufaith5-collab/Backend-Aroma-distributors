76bf42aa2792c21b66d0e902d3ec1924716c3b98
kirukufaith5-collab <kirukufaith5@gmail.com>
2026-07-29 20:29:36 +0300

    Create db.table and blueprints for farmer, admin and auth

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from config import Config
+
+#Initialize database and schema extensions
+db =SQLAlchemy()
+ma =Marshmallow()
+
+def create_app():
+    app =Flask(__name__)
+    app.config.from_object(Config)
+
+    #Bind extensions to app
+    db.init_app(app)
+    ma.init_app(app)
+
+    #Register blueprints for routes
+    from app.Admin.routes import admin_bp
+    from app.Farmer.routes import farmer_bp
+    from app.Auth.routes import auth_bp
+
+    app.register_blueprint(admin_bp,url_prefix='/admin')
+    app.register_blueprint(farmer_bp,url_prefix='/farmer')
+    app.register_blueprint(auth_bp,url_prefix='/auth')
+
+    #Create database tables
+    with app.app_context()
+     db.create_all()
+
+     return app
