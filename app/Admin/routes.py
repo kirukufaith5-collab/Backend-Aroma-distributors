9f290b845528f626741b7c7409ccb0028b0ce847
kirukufaith5-collab <kirukufaith5@gmail.com>
2026-07-30 19:07:41 +0300

    Add PUT method to update all batches

--- app/Admin/routes.py
+++ app/Admin/routes.py
@@ -20,3 +20,18 @@ def get_all_batches():
             'status': b.status
         })
     return jsonify(output), 200
+
+
+# 2. PUT: Change batch status (Approve / Reject)
+
+@admin_bp.route('/batches/<int:batch_id>/status', methods=['PUT'])
+def update_batch_status(batch_id):
+    # Find batch by ID
+    batch = ProductBatch.query.get_or_404(batch_id)
+    data = request.get_json()
+    
+    # Update status value
+    batch.status = data.get('status', batch.status)
+    db.session.commit()
+    
+    return jsonify({'message': f'Batch status updated to {batch.status}'}), 200
