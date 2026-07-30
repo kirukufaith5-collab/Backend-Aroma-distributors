46a751f2ce8c2c2c9971c41c042ca8d9ee8e4a8f
kirukufaith5-collab <kirukufaith5@gmail.com>
2026-07-30 20:05:07 +0300

    Refactor schema.py file

--- app/schemas.py
+++ app/schemas.py
@@ -1,21 +1,71 @@
 from app import ma
-from app.models import User, Batch,Order
+from app.models import Farmer, Admin, Client, ProductBatch, ClientOrder, OrderedItem, Payout
 
-#Schema to serialize Batch data
-class BatchSchema(ma.SQLAlchemyAutoSchema):
+# 1. Farmer Schema
+class FarmerSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        model = Batch
+        model = Farmer
         load_instance = True
 
-#Schema to serialize Order data
-class OrderSchema(ma.SQLAlchemyAutoSchema):
+farmer_schema = FarmerSchema()
+farmers_schema = FarmerSchema(many=True)
+
+
+# 2. Admin Schema
+class AdminSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        model =Order
-        load_instance =True
+        model = Admin
+        load_instance = True
 
-#Instantiate schemas
-batch_schema =BatchSchema()
-batches_schema =BatchSchema(many=True)
+admin_schema = AdminSchema()
+admins_schema = AdminSchema(many=True)
 
-order_schema =OrderSchema()
-orders_schema =OrderSchema(many =True)
+
+# 3. Client Schema
+class ClientSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Client
+        load_instance = True
+
+client_schema = ClientSchema()
+clients_schema = ClientSchema(many=True)
+
+
+# 4. ProductBatch Schema
+class ProductBatchSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = ProductBatch
+        load_instance = True
+
+batch_schema = ProductBatchSchema()
+batches_schema = ProductBatchSchema(many=True)
+
+
+# 5. ClientOrder Schema
+class ClientOrderSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = ClientOrder
+        load_instance = True
+
+order_schema = ClientOrderSchema()
+orders_schema = ClientOrderSchema(many=True)
+
+
+# 6. OrderedItem Schema (Many-to-Many Join Table)
+class OrderedItemSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = OrderedItem
+        load_instance = True
+
+ordered_item_schema = OrderedItemSchema()
+ordered_items_schema = OrderedItemSchema(many=True)
+
+
+# 7. Payout Schema
+class PayoutSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Payout
+        load_instance = True
+
+payout_schema = PayoutSchema()
+payouts_schema = PayoutSchema(many=True)
